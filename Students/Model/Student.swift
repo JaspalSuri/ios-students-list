cad9eda3b618c8d6216677af74cbd53fca7abe1d
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-02 21:48:36 -0800

    basic project setup

--- Students/Model/Student.swift
+++ Students/Model/Student.swift
@@ -1,11 +1,3 @@
-//
-//  Student.swift
-//  Students
-//
-//  Created by Andrew R Madsen on 8/5/18.
-//  Copyright © 2018 Lambda Inc. All rights reserved.
-//
-
 import Foundation
 
 struct Student: Codable {
